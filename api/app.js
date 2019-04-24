02d70b561dc27e937957aa11cd38b97aae34768d
pedro <pedroluismontesinosrivera@gmail.com>
2019-04-24 18:18:14 -0400

    #

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(helmet()); 
 
 app.use("/api/auth", AuthRouter);
 app.use("/api/users", UsersRouter);
